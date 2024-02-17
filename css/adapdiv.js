350e874c0e3c8493a505a8885246ec81517bbddf
12345 <a12345@MacBook-Pro-12345.local>
2024-02-17 11:06:16 +0300

    first commit

---
+++ css/adapdiv.js
@@ -0,0 +1,13 @@
+const burgermenu=document.querySelector(".burgermenu")
+const main=document.querySelector(".main")
+burgermenu.addEventListener("click",(e)=>{
+    if (div.getAttribute("class") === "burgermenu"){
+        div.removeAttribute("class")
+        div.setAttribute("class","circle")
+    }else{
+        div.removeAttribute("class")
+        div.setAttribute("class","burgermenu")
+    }
+})
+   
+
